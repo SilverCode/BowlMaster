34be39ac13978a629c54b1aee3d9c5c64efec50b
Ross Simpson <simpson.ross@gmail.com>
2016-12-19 19:42:52 +0200

    Add swiper to clear pins

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    public float standingThreshold = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    print(name + " " + isStanding());
+	}
+
+    public bool isStanding()
+    {
+        if (transform.eulerAngles.x < standingThreshold && transform.eulerAngles.z < standingThreshold)
+            return true;
+
+        return false;
+    }
+}
